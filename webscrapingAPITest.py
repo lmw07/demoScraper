c1a7a83105d0db21d9d876e6318d60ec6c6759b8
lmw07 <lane.m.wilson00@gmail.com>
2022-10-12 00:06:45 -0600

    integrated with webscrapingapi

---
+++ webscrapingAPITest.py
@@ -0,0 +1,65 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+from bs4 import BeautifulSoup
+import json
+import requests
+
+
+def rentler():
+    listings = []
+    base_url = 'https://api.webscrapingapi.com/v1'
+    params = {"api_key":"JBRtIH5Jk4GGhksdLEFlahaY4M3iWzEt", "url": 'https://www.rentler.com/places-for-rent/zip/84102/'}
+    response = requests.request('GET', base_url, params=params)
+    content=response.text
+    soup = BeautifulSoup(content, features='html.parser')
+    for element in soup.findAll('div', attrs={'class': 'col-12 col-lg-6'}):
+        listing = []
+        price = element.find('h3', attrs={'class': 'price'})
+        address = element.find('div', attrs={'class': 'address'})
+
+        if price and price.text:
+            listing.append(price.text)
+        else:
+            listing.append('No display data')
+
+        if address and address.text:
+            listing.append(address.text)
+        else:
+            listing.append('No display data')
+        listings.append(listing)
+    with open("rentlerListings.json", "w") as write_file:
+        json.dump(listings, write_file)
+
+
+def redfin():
+    listings = []
+    base_url = 'https://api.webscrapingapi.com/v1'
+    params = {"api_key": "Jbdkg6N2W1QEtw2JFs84CmfCeOozu4Nj",
+              "url": 'https://www.redfin.com/zipcode/84101'}
+    response = requests.request('GET', base_url, params=params)
+    content = response.text
+    soup = BeautifulSoup(content, features='html.parser')
+    for element in soup.findAll('div', attrs={'class': 'HomeCardContainer defaultSplitMapListView'}):
+        price = element.find('span', attrs={'class': 'homecardV2Price'})
+        address = element.find('span', attrs={'class': 'collapsedAddress primaryLine'})
+        listing = []
+        if price and price.text:
+            listing.append(price.text)
+        else:
+            listing.append('No display data')
+
+        if address and address.text:
+            listing.append(address.text)
+        else:
+            listing.append('No display data')
+        listings.append(listing)
+    with open("redfinListings.json", "w") as write_file:
+        json.dump(listings, write_file)
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+redfin()
