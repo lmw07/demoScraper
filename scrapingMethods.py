9b6f47aff9048f0f03aff984210142fe60d0cd57
lmw07 <lane.m.wilson00@gmail.com>
2022-10-24 23:43:54 -0600

    fixed bug where redfin didnt pick up the first item

--- scrapingMethods.py
+++ scrapingMethods.py
@@ -23,7 +23,10 @@ def RedfinZipSearch(zipcode):
     content = response.text
     soup = BeautifulSoup(content, features='html.parser')
     soup = BeautifulSoup(content, features='html.parser')
-    for element in soup.findAll('div', attrs={'class': 'HomeCardContainer defaultSplitMapListView isRentals'}):
+    if response.status_code == 200:
+        elements = soup.findAll('div', attrs={'class': 'HomeCardContainer defaultSplitMapListView isRentals'})
+    elements.append(soup.findAll('div', attrs={'class': 'HomeCardContainer selectedHomeCard defaultSplitMapListView isRentals'})[0])
+    for element in elements:
         price = element.find('span', attrs={'class': 'homecardV2Price'})
         address = element.find('span', attrs={'class': 'fullAddress'})
         if not element.find('span', attrs={'class': 'fullAddress'}):
@@ -47,37 +50,4 @@ def RedfinZipSearch(zipcode):
     res = json.dumps(listingArr, default=vars)
     return res
 
-def RedfinCitySearch(cityName):
-    listingArr = []
-    urlHeader = 'https://www.redfin.com'
-    base_url = 'https://api.webscrapingapi.com/v1'
-    params = {"api_key": "Jbdkg6N2W1QEtw2JFs84CmfCeOozu4Nj",
-              "url": f'https://www.redfin.com/zipcode/{zipcode}/apartments-for-rent'}
-    response = requests.request('GET', base_url, params=params)
-    content = response.text
-    soup = BeautifulSoup(content, features='html.parser')
-    soup = BeautifulSoup(content, features='html.parser')
-    for element in soup.findAll('div', attrs={'class': 'HomeCardContainer defaultSplitMapListView isRentals'}):
-        price = element.find('span', attrs={'class': 'homecardV2Price'})
-        address = element.find('span', attrs={'class': 'fullAddress'})
-        if not element.find('span', attrs={'class': 'fullAddress'}):
-            address = element.find('span', attrs={'class': 'collapsedAddress primaryLine'})
-        stats = element.find('div', attrs={'class': 'HomeStatsV2 font-size-small'})
-        bedrooms = stats.contents[0]
-        bathrooms = stats.contents[1]
-        size = stats.contents[2]
-        temp = element.find('img', attrs={'class': 'homecard-image'})
-        if len(temp["class"]) == 1:
-            pic = temp['src']
-        else:
-            pic = temp['data-src']
-        link = element.find('a', attrs={'class': 'slider-item'})['href']
-        if (price and price.text) and (pic) and (address and address.text) and link:
-            listing = listingClass.Listing(address.text, getNums(price.text), (urlHeader + link), pic)
-            listing.setSize(getNums(size.text))
-            listing.setBedrooms(getNums(bedrooms.text))
-            listing.setBathrooms(getNums(bathrooms.text))
-        listingArr.append(listing)
-    res = json.dumps(listingArr, default=vars)
-    return res
-print(RedfinZipSearch(64801))
+print(RedfinZipSearch(84102))
